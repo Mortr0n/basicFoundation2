83a9227f778438bbf7d1a67e0ea5f9dc5e484e22
Chris <cmsl4y3r@gmail.com>
2021-06-25 16:31:31 -0600

    Complete!

--- basicFoundation2.js
+++ basicFoundation2.js
@@ -187,7 +187,17 @@ function swapTowardTheCenter(arr){  //this one was hard for me.  I realized afte
     }
     return arr;
 }
-var testSwapTowardTheCenter = swapTowardTheCenter([1,2,3,4,5,6]);
-console.log(testSwapTowardTheCenter);
-var newTest = swapTowardTheCenter([1,2,3,4,5,6,7,8,9,10,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,0,0,0,0,0,0,0,0,0]);
-console.log(newTest);
+// var testSwapTowardTheCenter = swapTowardTheCenter([1,2,3,4,5,6]);
+// console.log(testSwapTowardTheCenter);
+// var newTest = swapTowardTheCenter([1,2,3,4,5,6,7,8,9,10,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,0,0,0,0,0,0,0,0,0]);
+// console.log(newTest);
+
+function scaleTheArray(arr, num){
+    for(var i=0; i<arr.length; i++){
+        arr[i]*=num;
+    }
+    return arr;
+}
+
+var testScaleTheArray = scaleTheArray([1,2,3,4,5,6,7], 20);
+console.log(testScaleTheArray);
